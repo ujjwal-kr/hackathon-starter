9e6f5e3d60a90fa56b442fa74fb18c01861ef27f
Ujjwal <epicujjwal@protonmail.com>
2022-08-11 01:15:05 +0530

    server: install deps

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from Express")
+})
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Server is Running")
+})
